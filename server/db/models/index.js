9923f937c8205c4fefe429e8473ed75fff757239
Alex <alexrgura@gmail.com>
2019-03-13 16:35:55 -0500

    made associations with as notes

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,8 +1,8 @@
 const User = require('./user')
 const Notes = require('./notes')
 
-Notes.belongsToMany(Notes, {through: 'note-note'})
-Notes.belongsToMany(Notes, {through: 'note-note'})
+Notes.belongsToMany(Notes, {as: 'Note-1', through: 'note-note'})
+Notes.belongsToMany(Notes, {as: 'Note-2', through: 'note-note'})
 
 Notes.belongsTo(User)
 User.hasMany(Notes)
