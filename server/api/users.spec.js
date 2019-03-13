7b86c8a154c17bf1ce9423a187ff664a56b9ea58
Alex <alexrgura@gmail.com>
2019-03-13 16:42:23 -0500

    fixed test

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -13,10 +13,12 @@ describe('User routes', () => {
 
   describe('/api/users/', () => {
     const codysEmail = 'cody@puppybook.com'
+    const password = 'bones'
 
     beforeEach(() => {
       return User.create({
-        email: codysEmail
+        email: codysEmail,
+        password
       })
     })
 
