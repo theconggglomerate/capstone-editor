55d69c9e65c922c40bfb5d6291c0e07337bc7539
pfgallagher <pfgallagher@gmail.com>
2019-03-14 11:29:18 -0500

    Add single note express route

--- server/api/notes.js
+++ server/api/notes.js
@@ -2,10 +2,30 @@ const router = require('express').Router()
 const {Notes} = require('./../db/models')
 module.exports = router
 
+router.param('noteId', async (req, res, next, id) => {
+  try {
+    const note = await Notes.findByPk(id)
+    if (note) {
+      req.note = note
+      next()
+    } else res.status(404).send('404 Error: Note Not Found')
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.get('/', async (req, res, next) => {
   try {
     const notes = await Notes.findAll()
     res.json(notes)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:noteId', (req, res, next) => {
+  try {
+    res.send(req.note)
   } catch (error) {
     next(error)
   }
