e13c80a16f130e1ed1bca2b99994d95421c50df8
pfgallagher <pfgallagher@gmail.com>
2019-03-18 15:49:50 -0500

    Implement planned suggestion functionality and highlight matched words in query

--- client/components/AllNotes.js
+++ client/components/AllNotes.js
@@ -9,10 +9,41 @@ class AllNotes extends Component {
       <div>
         <DataSearch
           componentId="noteSearch"
-          dataField={['title']}
-          onValueSelected={(value, cause, source) =>
-            this.props.history.push(`/notes/${source.id}`)
-          }
+          dataField={['title', 'content.cells.content']}
+          highlight={true}
+          highlightField={['title', 'content.cells.content']}
+          customHighlight={props => ({
+            highlight: {
+              pre_tags: ['<strong>'],
+              post_tags: ['</strong>'],
+              fields: {
+                title: {},
+                'content.cells.content': {}
+              }
+            }
+          })}
+          onSuggestion={suggestion => {
+            return {
+              label: `${
+                suggestion.highlight && suggestion.highlight.title
+                  ? suggestion.highlight.title
+                  : suggestion._source.title
+              }${
+                suggestion.highlight &&
+                suggestion.highlight['content.cells.content']
+                  ? `: ${suggestion.highlight['content.cells.content']}`
+                  : ''
+              }`,
+              value: suggestion,
+              source: suggestion._source
+            }
+          }}
+          fuzziness={0}
+          onValueSelected={(value, cause, source) => {
+            if (cause === 'SUGGESTION_SELECT')
+              this.props.history.push(`/notes/${source.id}`)
+            if (cause === 'ENTER_PRESS') console.log(value)
+          }}
         />
       </div>
     ) : (
