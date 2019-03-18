4acbca4ca6310f65dd51e004be950944d69fca2d
Alexandra Gerard <alexandralbgerard@gmail.com>
2019-03-18 15:49:02 -0500

    added wrapper and for redux persist. whitelisted editor store.

--- client/store/index.js
+++ client/store/index.js
@@ -4,11 +4,15 @@ import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import notes from './notes'
+import {persistStore, persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2'
 
 const reducer = combineReducers({
   notes,
   user
 })
+
 const middleware = composeWithDevTools(
   applyMiddleware(
     thunkMiddleware,
@@ -17,7 +21,18 @@ const middleware = composeWithDevTools(
     })
   )
 )
-const store = createStore(reducer, middleware)
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2,
+  whitelist: ['editor']
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+const store = createStore(persistedReducer, middleware)
+export const persistor = persistStore(store)
 
 export default store
 export * from './user'
