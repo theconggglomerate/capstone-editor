412d3478b56ae2ff83ccc12e26188a93d57c3ac4
pfgallagher <pfgallagher@gmail.com>
2019-03-21 14:52:24 -0500

    Yet another attempt at fixing this bug.

--- server/api/es.js
+++ server/api/es.js
@@ -6,7 +6,7 @@ router.use(
   proxy({
     target: process.env.BONSAI_URL || 'http://localhost:9200',
     changeOrigin: true,
-    ws: true,
+    auth: `${process.env.BONSAI_USERNAME}:${process.env.BONSAI_PASSWORD}`,
     pathRewrite: {'^/api/es': process.env.BONSAI_URL || 'http://localhost:9200'}
   })
 )
